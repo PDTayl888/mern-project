d1e82ef6ae46a5799c138a2ba6f83168888455e9
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-30 19:40:31 -0400

    added categoryRoutes

---
+++ backend/routes/categoryRoutes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const Category = require('../models/Category');
+const { authMiddleware } = require('../middleware/authMiddleware');
+
+router.get('/', authMiddleware, protect, async (req, res) => {
+  try {
+    const categories = await Category.find({ user: req.user.id });
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ message: 'FAILed TO FETCH CATEGORY' });
+  }
+});
+
+router.post('/', protect, async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const category = await Category.create({
+      name,
+      description,
+      user: req.user.id
+    });
+    
+    res.status(201).json(category);
+  } catch (error) {
+    res.status(400).json({ message: 'FAILED TO FETCH CATEGORY' });
+  }
+});
+
+router.delete('/:id', protect, async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return res.status(404).json({ message: 'CATEGORY NOT FOUND' });
+    }
+    if (category.user.toString() !== req.user.id) {
+      return res.status(401).json({ message: 'USER NOT AUTHORIZED' });
+    }
+
+    await category.deleteOne();
+    res.status(200).json({ id: req.params.id });
+  } catch (error) {
+    res.status(500).json({ message: 'FAILED TO DELETE' });
+  }
+});
