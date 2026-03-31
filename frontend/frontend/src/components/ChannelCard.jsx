64a38b86a655e83f8655a9e160d82b1e0905bfb4
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-31 15:22:58 -0400

    work on ChannelCard editing

--- frontend/frontend/src/components/ChannelCard.jsx
+++ frontend/frontend/src/components/ChannelCard.jsx
@@ -1,27 +1,76 @@
 import { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const ChannelCard = ({ card }) => {
+const ChannelCard = ({ card, onDelete }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editData, setEditData] = useState({
+    channelName: card.channelName,
+    youTubeUrl: card.youTubeUrl,
+    description: card.description,
+  });
+
+  const handleChange = (e) => {
+    setEditData({...editData, e.target.name: e.target.value});
+  };
+
+  const handleSave = () => {
+  };
+
+  const handleCancel = () => {
+    setEditData({
+        channelName: card.channelName,
+        youTubeUrl: card.youTubeUrl,
+        description: card.description,
+    });
+    setIsEditing(false);
+  };
+
   return (
     <li>
-      <div>
-        <h3>{card.channelName}</h3>
-        <p>
-          <a href={card.youTubeUrl}>{card.youTubeUrl}</a>
-        </p>
-        <p>{card.description}</p>
-
+      {isEditing ? (
         <div>
-          <label>status: </label>
-          <select value={card.status}>
-            <option value="Subscribed">Subscribed</option>
-            <option value="Watch Later">Watch Later</option>
-            <option value="Archived">Archived</option>
-          </select>
-        </div>
+          <input
+            type="text"
+            name="channelName"
+            value={editData.channelName}
+            onChange={handleChange}
+          />
+          <input
+            type="url"
+            name="youTubeUrl"
+            value={editData.youTubeUrl}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="description"
+            value={editData.description}
+            onChange={handleChange}
+          />
 
-        
-      </div>
+          <button onClick={handleSave}>save changes</button>
+          <button onClick={handleCancel}>cancel</button>
+        </div>
+      ) : (
+        <div>
+          <h3>{card.channelName}</h3>
+          <p>
+            <a href={card.youTubeUrl}>{card.youTubeUrl}</a>
+          </p>
+          <p>{card.description}</p>
+
+          <div>
+            <label>status: </label>
+            <select value={card.status}>
+              <option value="Subscribed">Subscribed</option>
+              <option value="Watch Later">Watch Later</option>
+              <option value="Archived">Archived</option>
+            </select>
+          </div>
+          <button onClick={() => setIsEditing(true)}>Edit Text</button>
+          <button onClick={() => onDelete(card._id)}>Remove</button>
+        </div>
+      )}
     </li>
   );
 };
