4ba05ebdefa626a2e7e376c7d731330b825d201e
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-31 14:01:50 -0400

    edited ChannelCard component

--- frontend/frontend/src/components/ChannelCard.jsx
+++ frontend/frontend/src/components/ChannelCard.jsx
@@ -1,9 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const ChannelCard = () => {
-    return (
-        <>
+const ChannelCard = ({ card }) => {
+  return (
+    <li>
+      <div>
+        <h3>{card.channelName}</h3>
+        <p>
+          <a href={card.youTubeUrl}>{card.youTubeUrl}</a>
+        </p>
+        <p>{card.description}</p>
+
+        <div>
+          <label>status: </label>
+          <select value={card.status}>
+            <option value="Subscribed">Subscribed</option>
+            <option value="Watch Later">Watch Later</option>
+            <option value="Archived">Archived</option>
+          </select>
+        </div>
+
         
-        </>
-    )
-}
+      </div>
+    </li>
+  );
+};
+
+export default ChannelCard;
