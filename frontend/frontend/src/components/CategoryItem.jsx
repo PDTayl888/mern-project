5b3c76c94780b60653ef16c438ecc0fdbb08d538
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-04-01 15:17:35 -0400

    edited CategoryItem

--- frontend/frontend/src/components/CategoryItem.jsx
+++ frontend/frontend/src/components/CategoryItem.jsx
@@ -1,57 +1,49 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const CategoryItem = ({ category }) => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [editName, setEditName] = useState(category.name);
-    const [editDesc, setEditDesc] = useState(category.description);
+const CategoryItem = ({ category, onDelete, onUpdate }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editName, setEditName] = useState(category.name);
+  const [editDesc, setEditDesc] = useState(category.description);
 
-    const handleEditName = (e) => {
-        setEditName(e.target.value);
-    };
+  const handleEditName = (e) => {
+    setEditName(e.target.value);
+  };
 
-    const handleEditDesc = (e) => {
-        setEditDesc(e.target.value);
-    };
+  const handleEditDesc = (e) => {
+    setEditDesc(e.target.value);
+  };
 
-    const handleSave = () => {
+  const handleSave = () => {
+    onUpdate(category._id, { name: editName, description: editDesc });
+    setIsEditing(false);
+  };
 
-    };
+  const handleCancel = () => {
+    setEditName(category.name);
+    setEditDesc(category.description);
+    setIsEditing(false);
+  };
 
-    const handleCancel = () => {
-    
-        setIsEditing(false);
-    };
-
-
-
-    return (
- <li>
+  return (
+    <li>
       {isEditing ? (
         <div>
-          <input
-            type="text"
-            value={editName}
-            onChange={handleEditName}
-          />
-          <input
-            type="text"
-            value={editDesc}
-            onChange={handleEditDesc}
-          />
+          <input type="text" value={editName} onChange={handleEditName} />
+          <input type="text" value={editDesc} onChange={handleEditDesc} />
           <button onClick={handleSave}>save</button>
           <button onClick={handleCancel}>cancel</button>
         </div>
       ) : (
         <div>
           <h3>{category.name}</h3>
-          
+
           <button onClick={() => setIsEditing(true)}>Edit Text</button>
           <button onClick={() => onDelete(category._id)}>Delete</button>
         </div>
       )}
     </li>
-    );
+  );
 };
 
 export default CategoryItem;
