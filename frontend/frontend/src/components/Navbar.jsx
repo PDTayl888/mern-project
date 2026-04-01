12c95a3fc8bc053018c0f8eb087974be50d131d4
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-04-01 14:21:39 -0400

    edited Navbar

--- frontend/frontend/src/components/Navbar.jsx
+++ frontend/frontend/src/components/Navbar.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
-import { Link } from 'react-router-dom';
-
+import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
 
-    return (
+  if (!user) return null;
 
-    );
+  return (
+    <header>
+      <Link to="/">
+        <h2>sub_script_</h2>
+      </Link>
+
+      <div>
+        <span>Welcome, {user.username}</span>
+        <button onClick={logout}>Logout</button>
+      </div>
+    </header>
+  );
 };
+
+export default Navbar;
