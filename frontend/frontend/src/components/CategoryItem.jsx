038af624f826ed8b0907d7e1bab7a33e5702496c
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-04-01 14:46:28 -0400

    edited CategoryItem

--- frontend/frontend/src/components/CategoryItem.jsx
+++ frontend/frontend/src/components/CategoryItem.jsx
@@ -1,6 +1,36 @@
-import { useContext } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const CategoryItem = () => {
-    
+    const [isEditing, setIsEditing] = useState(false);
+    return (
+ <li>
+      {isEditing ? (
+        <div>
+          <input
+            type="text"
+            name="channelName"
+            value={editData.channelName}
+            onChange={handleChange}
+          />
+          <input
+            type="url"
+            name="youTubeUrl"
+            value={editData.youTubeUrl}
+            onChange={handleChange}
+          />
+          <button onClick={handleSave}>save changes</button>
+          <button onClick={handleCancel}>cancel</button>
+        </div>
+      ) : (
+        <div>
+          <h3>{card.channelName}</h3>
+          </div>
+          <button onClick={() => setIsEditing(true)}>Edit Text</button>
+          <button onClick={() => onDelete(card._id)}>Remove</button>
+        </div>
+      )}
+    </li>    );
 };
+
+export default CategoryItem;
