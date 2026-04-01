ba33b4e1d67241421a6bb13a8cd3b3c572d8b5fa
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-04-01 19:10:22 -0400

    started dashboard

---
+++ frontend/frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import useFetch from "../hooks/useFetch";
+import { fetchClient } from "../utils/apiClient";
+import CategoryItem from "../components/CategoryItem";
+
+const Dashboard = () => {};
+
+export default Dashboard;
