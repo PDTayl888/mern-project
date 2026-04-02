339c54d26cc50654f674feaae0b30a41e3a7c6b9
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-04-01 22:50:52 -0400

    added connection file

---
+++ backend/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("SUCCESSFUL CONNECTION TO MONGODB");
+  } catch (error) {
+    console.error("CONNECTION ERROR: ", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
