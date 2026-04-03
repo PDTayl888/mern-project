f1648f7fc1eabc0819d310b55274e9b1a78ab551
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-04-02 22:18:32 -0400

    backend maybe

--- frontend/frontend/src/pages/Login.jsx
+++ frontend/frontend/src/pages/Login.jsx
@@ -83,7 +83,7 @@ const Login = () => {
         <button
           onClick={() =>
             (window.location.href =
-              "https://yt-subscript.onrender.com/api/users/auth/github")
+              "https://mern-project-41xe.onrender.com/api/users/auth/github")
           }
         >
           LOGIN WITH GITHUB
